36289c5c85d943c5614930fa9251efd252e7a27c
joevert <joao_marcos99@hotmail.com>
2024-10-17 22:38:34 -0300

    Add get_player_input

--- main.rb
+++ main.rb
@@ -6,3 +6,18 @@ def generate_secret_code
   secret_code = Array.new(4) {COLORS.sample}
   puts secret_code
 end
+
+def get_player_input
+  puts "Choose 4 colors between: #{COLORS.join(', ')}"
+  puts "Input the 4 colors separated by space (ex: red blue yellow yellow)"
+
+  loop do
+    input = gets.chomp.downcase.split
+    if input.length == 4 && input.all? { |color| COLORS.include?(color)}
+      return input
+    else
+      puts "Invalid input. Please choose 4 valid colours."
+      puts "Try again (ex: yellow blue purple orange):"
+    end
+  end
+end
